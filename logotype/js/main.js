ae01047b36a04d11ad9112d053af01a6d8aaee3a
hvoyka <hvoinii@gmail.com>
2019-12-20 12:04:32 +0600

    add slick-slider, slider-block ready

--- logotype/js/main.js
+++ logotype/js/main.js
@@ -1,3 +1,21 @@
 $(function(){
+    $('.slider__inner').slick({
+        nextArrow: '<button type="button" class="slick-next slick-arrow-btn"></button>',
+        prevArrow: '<button type="button" class="slick-prev slick-arrow-btn"></button>',
+    });
+    sliderArrowsPos();
+    function sliderArrowsPos () {
+        //position for slider arrows, like they in container
+        let sliderItemContainer =  $('.slick-active .container');
+        let ofl = $(sliderItemContainer).offset().left;
+        console.log(sliderItemContainer, ofl);
+        $('.slick-prev').css("left", ofl+15);
+        $('.slick-next').css("right", ofl+15);
+        console.log('arrow pos');
+    }
     
+
+    window.addEventListener('resize', function(event){
+        sliderArrowsPos();
+      });
 });
