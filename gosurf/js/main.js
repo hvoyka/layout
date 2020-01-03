b013e78c8da98411dad5b3478b0318c6e5fe55d0
hvoyka <hvoinii@gmail.com>
2020-01-03 16:24:40 +0600

    add slider and style info block  in header

--- gosurf/js/main.js
+++ gosurf/js/main.js
@@ -1,6 +1,11 @@
 $(function(){
 
-
+$('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="">',
+});
 
   
 });
