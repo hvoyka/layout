0efe74d4ec7e9fb42bb25d19ae6b438b8cdb0a79
hvoyka <hvoinii@gmail.com>
2020-01-03 20:08:14 +0600

    add second slider in header, and attached it to main slider

--- gosurf/js/main.js
+++ gosurf/js/main.js
@@ -3,9 +3,15 @@ $(function(){
 $('.header__slider').slick({
     infinite: true,
     fade: true,
+    asNavFor: '.slider-dots',
     prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg" alt="">',
     nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="">',
 });
-
+$('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    focusOnSelect: true,
+    asNavFor: '.header__slider',
+});
   
 });
