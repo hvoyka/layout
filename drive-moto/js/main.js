8ff4e355b7931bedda231f687ec28b500e0d8c5a
Lavrentev Stanislav <hvoinii@gmail.com>
2020-08-09 18:38:51 +0600

    styles and js for slider in banner-section

--- drive-moto/js/main.js
+++ drive-moto/js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="./images/svg/slider-arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="./images/svg/slider-arrow-right.svg" alt=""></button>',
+  })
 });
