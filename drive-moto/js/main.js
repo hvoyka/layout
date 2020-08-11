b9ba5f1ae0f747eba9982407cc8f6e6dc6eb7282
Lavrentev Stanislav <hvoinii@gmail.com>
2020-08-11 14:10:32 +0600

    add universal classes to tabs, and change js to use it, one code for all tabs

--- drive-moto/js/main.js
+++ drive-moto/js/main.js
@@ -5,13 +5,13 @@ $(function(){
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="./images/svg/slider-arrow-left.svg" alt=""></button>',
     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="./images/svg/slider-arrow-right.svg" alt=""></button>',
   })
-  //MAIN PAGE TABS SEARCH
-  $('.search__tabs-item').on('click', function(e){
+  //TABS
+  $('.tab').on('click', function(e){
     e.preventDefault();
-    $('.search__tabs-item').removeClass('search__tabs-item--active');
-    $('.search__content-item').removeClass('search__content-item--active');
-    $(this).addClass('search__tabs-item--active');
-    $($(this).attr('href')).addClass('search__content-item--active');
+    $('.tab').removeClass('tab--active');
+    $('.tabs-content').removeClass('tabs-content--active');
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tabs-content--active');
   })
   
 });
