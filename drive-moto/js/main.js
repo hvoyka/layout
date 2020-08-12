4c6732963ebb4a812bfb3abcc26512171f4e304f
Lavrentev Stanislav <hvoinii@gmail.com>
2020-08-12 19:45:47 +0600

    complete product item, add styles for hover, add sale status

--- drive-moto/js/main.js
+++ drive-moto/js/main.js
@@ -4,7 +4,7 @@ $(function(){
     dots: true,
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="./images/svg/slider-arrow-left.svg" alt=""></button>',
     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="./images/svg/slider-arrow-right.svg" alt=""></button>',
-  })
+  });
   //TABS
   $('.tab').on('click', function(e){
     e.preventDefault();
@@ -12,6 +12,8 @@ $(function(){
     $('.tabs-content').removeClass('tabs-content--active');
     $(this).addClass('tab--active');
     $($(this).attr('href')).addClass('tabs-content--active');
-  })
-  
+  });
+  $('.product-item__favor-btn').on('click', function() {
+    $(this).toggleClass('product-item__favor-btn--active');
+  });
 });
