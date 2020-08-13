234021d0a0a5ed9d0e5253466eee1b469e9bfde9
Lavrentev Stanislav <hvoinii@gmail.com>
2020-08-13 14:09:13 +0600

    load product images, add product slider, fix different size of img box

--- drive-moto/js/main.js
+++ drive-moto/js/main.js
@@ -1,10 +1,15 @@
 $(function(){
-  //MAIN PAGE SLIDER
+  //MAIN TOP SLIDER
   $('.banner-section__slider').slick({
     dots: true,
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="./images/svg/slider-arrow-left.svg" alt=""></button>',
     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="./images/svg/slider-arrow-right.svg" alt=""></button>',
   });
+  //PRODUCTS SLIDER
+  $('.products-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 2
+  })  
   //TABS
   $('.tab').on('click', function(e){
     e.preventDefault();
